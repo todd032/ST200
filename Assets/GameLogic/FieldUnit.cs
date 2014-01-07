fa688498f5c43a3ca49f3dcb813899758fa974d9
Seung Ho Jang <shjang1992@hotmail.com>
2014-01-07 23:50:24 +0900

    Basic unit class commit

---
+++ Assets/GameLogic/FieldUnit.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+
+namespace WindGuardian
+{
+    public class FieldUnit {
+
+    }
+}
---
+++ Assets/UnityObject/UnityFieldUnit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldUnit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private string spineName;
+    private WindGuardian.FieldUnit logicUnit;
+}
